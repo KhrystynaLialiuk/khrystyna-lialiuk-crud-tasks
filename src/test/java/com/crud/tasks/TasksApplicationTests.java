822127b8daed6c748b95fdd897867a061016a4c3
KhrystynaLialiuk <k.lialiuk@gmail.com>
2019-08-14 17:45:34 +0200

    First Controller Method

--- src/test/java/com/crud/tasks/TasksApplicationTests.java
+++ src/test/java/com/crud/tasks/TasksApplicationTests.java
@@ -1,4 +1,4 @@
-package com.crud.tasks;
+/*package com.crud.tasks;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,9 +8,4 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TasksApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
+}*/
