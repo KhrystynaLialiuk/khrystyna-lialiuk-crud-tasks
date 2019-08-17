7755b1e0ad6b3e7ee9fd2cc3e983d0ee003e3d77
KhrystynaLialiuk <k.lialiuk@gmail.com>
2019-08-17 23:07:01 +0200

    Delete method implementation

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.repository;
 
+import com.crud.tasks.controller.TaskNotFoundException;
 import com.crud.tasks.domain.Task;
 import org.springframework.data.repository.CrudRepository;
 
@@ -12,4 +13,10 @@ public interface TaskRepository extends CrudRepository<Task, Long> {
 
     @Override
     Optional<Task> findById(Long id);
+
+    @Override
+    Task save(Task task);
+
+    @Override
+    void deleteById(Long id);
 }
--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,11 +1,13 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.controller.TaskNotFoundException;
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DbService {
@@ -16,8 +18,19 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Task getTask(Long id) {
-        return repository.findById(id).get();
+    public Optional<Task> getTask(final Long id) {
+        return repository.findById(id);
     }
 
+    public Task saveTask(final Task task) {
+        return repository.save(task);
+    }
+
+    public void deleteTask(final Long id) throws TaskNotFoundException {
+        try {
+            repository.deleteById(id);
+        } catch (IllegalArgumentException e) {
+            throw  new TaskNotFoundException();
+        }
+    }
 }
