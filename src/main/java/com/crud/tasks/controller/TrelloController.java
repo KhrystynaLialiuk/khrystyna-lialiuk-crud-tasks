1de895b000ec67aee81dfee8cb675c615744d8bc
KhrystynaLialiuk <k.lialiuk@gmail.com>
2019-09-15 18:22:58 +0200

    First Spring Test

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -20,26 +20,8 @@ public class TrelloController {
     private TrelloClient trelloClient;
 
     @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
-    public void getTrelloBoards() {
-
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-
-        for (TrelloBoardDto trelloBoardDto : trelloBoards) {
-            if ((trelloBoardDto.getId() != null) & (trelloBoardDto.getName() != null)) {
-                if (trelloBoardDto.getName().toLowerCase().contains("kodilla")) {
-                    System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
-                }
-            }
-        }
-        System.out.println();
-
-        trelloBoards.forEach(trelloBoardDto -> {
-            System.out.println(trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
-            System.out.println("This board contains lists: ");
-            trelloBoardDto.getLists().forEach(trelloList ->
-                    System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
-
-        });
+    public List<TrelloBoardDto> getTrelloBoards() {
+        return trelloClient.getTrelloBoards();
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "createTrelloCard")
